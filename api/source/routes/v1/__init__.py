f79f3550f8aa67d1801a1eadcbbf99570e4fd0ce
Ignacio Becerra <ignaciobecef@gmail.com>
2025-06-13 15:18:02 -0300

    Borrar archivos de priority para simplificar el proyecto

--- api/source/routes/v1/__init__.py
+++ api/source/routes/v1/__init__.py
@@ -1,10 +1,9 @@
 from fastapi import APIRouter
 
-from .priorities_routes import router as priorities_router
+
 from .tasks_routes import router as tasks_router
 
 
 router_v1 = APIRouter(prefix='/v1')
 
 router_v1.include_router(tasks_router, tags=['Tasks'])
-router_v1.include_router(priorities_router, tags=['Priorities'])
--- api/source/routes/v1/dependencies.py
+++ api/source/routes/v1/dependencies.py
@@ -1,10 +1,6 @@
 from api.source.controllers.tasks_controllers import TaskController
-from api.source.controllers.priorities_controllers import PrioritiesController
 from api.source.services.task_service import TaskService
-from api.source.services.priority_service import PriorityService
+
 
 task_service = TaskService(None)
 task_controller = TaskController(task_service)
-
-priorities_service = PriorityService(None)
-priority_controller = PrioritiesController(priorities_service)
